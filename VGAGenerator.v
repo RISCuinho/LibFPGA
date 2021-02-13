76a67c08e3c2225458287d40d80aaa65bfee4871
Carlos Delfino (WSL - Estudio - 4) <consultoria@carlosdelfino.eti.br>
2021-02-13 17:33:35 -0300

    corrigido erros gerais, ampliado parametros

--- VGAGenerator.v
+++ VGAGenerator.v
@@ -12,29 +12,32 @@ module VGAGenerator
                       // a cada clock pula para o próximo pixel exibivel
                       // [31-24] ignorado, [23-16]Red, [15-8]Green, [7-0]Blue
                       // para saber qual pixel a ser obtido usar COL, LINE e inDisplayArea
-  output reg [DEEP_COLOR - 1: 0]R, 
-  output reg [DEEP_COLOR - 1: 0]G, 
-  output reg [DEEP_COLOR - 1: 0]B, 
+  output reg [DEEP_COLOR - 1: 0]R = {DEEP_COLOR{1'b0}}, 
+  output reg [DEEP_COLOR - 1: 0]G = {DEEP_COLOR{1'b0}}, 
+  output reg [DEEP_COLOR - 1: 0]B = {DEEP_COLOR{1'b0}}, 
   output VS, output HS,
-	output reg inDisplayArea,
-  output reg [9:0]COL, output reg [8:0]LINE
+  output reg [9:0]COL = 10'b0, output  reg [8:0]LINE = 9'b0,
+ 	output reg inDisplayArea = 1'b0
 );
 
 wire [9:0]local_col;
 wire [8:0]local_line;
 wire local_inDisplayArea;
+wire local_HS, local_VS;
 
 assign local_col = COL;
 assign local_line = LINE;
-assign local_InDisplayArea = inDisplayArea;
+assign local_inDisplayArea = inDisplayArea;
+assign local_HS = HS;
+assign local_VS = VS;
 
 // Gera os sinais de sincronismo
-VGASync #(.RES_X(RES_X), .RES_Y(RES_Y)) sync(.clk(clk), .COL(local_col), .LINE(local_line), .VS(VS), .HS(HS), .inDisplayArea(local_inDisplayArea));
+VGASync #(.RES_X(RES_X), .RES_Y(RES_Y)) sync(.clk(clk), .COL(local_col), .LINE(local_line), .VS(local_VS), .HS(local_HS), .inDisplayArea(local_inDisplayArea));
 
 // mapea o byte a sua respectiva cor, confrome a profundida de cores (DEEP_COLOR)
 always @(posedge clk)
 begin
-	if(VS && HS)
+	if(local_VS && local_HS && local_inDisplayArea)
 	begin
     // ainda a ser implementado
 		R <= pixel[7:0];
@@ -58,7 +61,6 @@ B (us)                 3.77      Sync pulse lenght
 C (us)                 1.89      Back porch
 D (us)                 25.17     Active video time
 E (us)                 0.94      Front porch
-         ....... pixels .......
          ______________________          ________
 ________|        VIDEO         |________| VIDEO (next line)
     |-C-|----------D-----------|-E-|
@@ -121,26 +123,51 @@ module VGASync
 #(
   parameter RES_X = 640,
   parameter RES_Y = 480
-)
-(
+)(
   input clk, 
-  output reg [9:0] COL, 
-  output reg [8:0] LINE,
+  output reg [9:0] COL = 10'b0, 
+  output reg [8:0] LINE = 9'b0,
+  output reg inDisplayArea = 1'b0,
   output VS,
-  output HS,
-  output reg inDisplayArea
+  output HS
   );
 
-wire MAX_COL = (COL == 767); // tentando entender porque 767 (9'h2FF) 
-                              // não deveria ser 799 (800)
-reg local_HS, local_VS;
+localparam SIZE_HS = 96;
+localparam LIMIT_MAX_COL =  (RES_X + 8 + SIZE_HS + 8 + 40  + 8) -1; // -1 porque a contagem começa de zero
+// 640 pixels video
+//  8 pixels left border
+//  8 pixels front porch
+// 96 pixels horizontal sync
+// 40 pixels back porch
+//  8 pixels right border
+//===============================
+// 800 pixels 
+localparam SIZE_VS = 20;
+localparam LIMIT_MAX_LINE = (2 + 2 + 25 + 8 + RES_Y + 8);
+//  2 lines front porch
+//  2 lines vertical sync
+// 25 lines back porch
+//  8 lines top border
+//480 lines video
+//  8 lines bottom border
+//---
+//525 lines total per field
+
+initial begin
+  $display("Iniciado VGA Sync Generator!");
+  $display("LIMIT_MAX_COL: %D, Size HS: %D", LIMIT_MAX_COL, SIZE_HS);
+  $monitor("MAX_COL: %D",MAX_COL);
+  //$stop();
+end
+
+wire MAX_COL = (COL == LIMIT_MAX_COL);
 
 always @(posedge clk)
 begin: COUNT_COL
-if(MAX_COL)
-  COL <= 0;
-else
-  COL <= COL + 1;
+  if(MAX_COL)
+    COL <= 0;
+  else
+    COL <= COL + 1;
 end
 
 always @(posedge clk)
@@ -149,10 +176,11 @@ if(MAX_COL)
     LINE <= LINE + 1;  
 end
 
+reg local_HS, local_VS;
 always @(posedge clk)
 begin: SYNC
-  local_HS <= (COL[9:4] == 6'h2D);// active for 16 clocks  - 6'h2D - 9'h168 - 360 
-  local_VS <= (LINE == 500);    // active for 768 clocks - 500
+  local_HS <= (COL >= (LIMIT_MAX_COL - SIZE_HS) && COL <= LIMIT_MAX_COL); 
+  local_VS <= (LINE == LIMIT_MAX_LINE);    
 end
 
 // padrão industrial do VGA usa sincronismo inverso
