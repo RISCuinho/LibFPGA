8c7cef7765b34ba89c22f94d09b981686b996833
Carlos Delfino (WSL - Estudio - 4) <consultoria@carlosdelfino.eti.br>
2021-03-14 01:25:10 -0300

    criado um Contador Binário do tipo LED.
    Contador Binário do tipo CLED:
    
    CB*CLED
      - Counter
      - Binary
      - 4/2-bit (* -> N bits)
      - C (asynchronous reset/Clear)
      - Load
      - E (chip Enable)
      - D (up/Down)
    
      https://www.vicilogic.com/vicilearn/run_step/?c_id=22&c_pos=286

---
+++ bc.v
@@ -0,0 +1,34 @@
+/*
+ The CB*CLED component naming convention is
+  - Counter  
+  - Binary                  
+  - 4/2-bit
+  - C (asynchronous reset/Clear)   
+  - Load        
+  - E (chip Enable)     
+  - D (up/Down)
+
+  https://www.vicilogic.com/vicilearn/run_step/?c_id=22&c_pos=286
+*/
+module BinaryCounter #(
+   parameter WIDTH = 4
+) (
+   input                   clk, rst,
+   output reg [WIDTH-1:0]  loadData, counter,
+   input                   E, D, load
+);
+
+   always @(posedge rst) begin
+      counter <= {WIDTH{1'b0}};
+   end
+
+   always @(posedge clk) begin
+      if(!rst && E && !load)
+      begin
+        counter <= D ? counter + 1 : counter - 1;
+      end
+      else if(!rst && load)
+         counter <= loadData;      
+      
+   end
+endmodule
