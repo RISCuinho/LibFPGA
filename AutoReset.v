b65fe3ef9eeab1aa139092ebbe2779dc3427f3e3
Carlos Delfino - Acer - Estudio 3 <consultoria@carlosdelfino.eti.br>
2021-07-02 00:31:52 -0300

    Adicionado parametro para inverter RST

--- AutoReset.v
+++ AutoReset.v
@@ -1,4 +1,6 @@
-module AutoReset (
+module AutoReset #(
+   parameter INVERT_RST = 1'b0
+)(
 input clk,
 input [31:0] count,
 input clr,
@@ -12,12 +14,12 @@ always @(negedge clk)
 	if (clr) 
 	begin
 		clk_count <= 1'b1;
-		rst <= 1'b1;
+		rst <= !INVERT_RST;
 	end
-	else if (clk_count > count) rst <= 1'b0;
+	else if (clk_count > count) rst <= INVERT_RST;
 	else 
 	begin 
 		clk_count <= clk_count + 1;
-		rst <= 1'b1;
+		rst <= !INVERT_RST;
 	end
 endmodule
